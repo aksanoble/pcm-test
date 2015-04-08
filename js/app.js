22cb6e89eb3903c76461f257705f2fe2209e1125
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-04-08 16:40:13 +0530

    Fix Backbone localstorage issue

--- js/app.js
+++ js/app.js
@@ -1,51 +1,53 @@
 $(function () {
   var app = app || {};
 
-//Defining Model
-  app.Key = Backbone.Model.extend({});
-
-  // Defining Collection
-  app.Keys = Backbone.Collection.extend({
-
-    model: app.Key,
+  //Defining Model
+  app.Key = Backbone.Model.extend({
+    defaults: {
+      key: [0, 5, 10]
+    },
 
     localStorage: new Backbone.LocalStorage("pcm-matrix")
-
   });
 
-// Main View
+
+  // Main View
   app.View = Backbone.View.extend({
 
     el: '#mainview',
-    //template: _.template($('#mymodel-template').html()),
 
     events: {
       'click td': 'updateKey'
     },
 
     initialize: function () {
-      this.collection.fetch();
-      return this.render();
+      this.model.on('all', this.render, this);
+
+      this.model.fetch()
+        .then(function () {
+          this.render();
+        }.bind(this));
     },
     updateKey: function (e) {
       var $cellIndex = $("td").index(e.target);
-      var matrixKey = _.clone(this.collection.find('key')); //Using clone as if referred to the same  object does not trigger changed event
+      var matrixKey = _.clone(this.model.get('key'));
       matrixKey[Math.floor($cellIndex / 5)] = $cellIndex;
-      this.collection.create({key: matrixKey});
+      this.model.save({key: matrixKey});
     },
 
     addClass: function (cell) {
-      var $cellEl = $("td:eq(" + cell + ")");
-      $cellEl.nextAll().removeClass("checked");
-      $cellEl.prevAll().addBack().addClass("checked");
+      console.log(cell);
+      var $cell = $("td:eq(" + cell + ")");
+      $cell.nextAll().removeClass("checked");
+      $cell.prevAll().addBack().addClass("checked");
     },
 
     render: function () {
-      var matrixCell = this.collection.find('key');
+      var matrixCell = this.model.get('key');
       _.each(matrixCell, this.addClass);
-      return this;
+
     }
   });
 
-  var App = new app.View({collection: new app.Keys()});
+  var App = new app.View({model: new app.Key({id: 27182})});
 });
