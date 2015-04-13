550c7c763bf223fb979fe20d9b79b1482f52881a
aksanoble <aksanoble@gmail.com>
2015-04-13 18:27:50 +0530

    initialization fix

--- js/app.js
+++ js/app.js
@@ -13,37 +13,46 @@ $(function () {
   var subscribeObj = {
     channel : 'test11',
     message : function( message, env, channel ){
-       // RECEIVED A MESSAGE.
-       console.log("message received");
-       var wireStat = message;
-       console.log(wireStat);
-       var currentStat = app.stat.get('stats');
-       console.log(currentStat);
-       console.log(_.reduce(_.first(wireStat, 4), function(sum, el) {
-           return sum + el;}, 0) > _.reduce(_.first(currentStat, 4), function(sum, el) {
-               return sum + el;}, 0) );
-       if (_.reduce(_.first(wireStat, 4), function(sum, el) {
-           return sum + el;}, 0) > _.reduce(_.first(currentStat, 4), function(sum, el) {
-               return sum + el;}, 0) ) {
-                 app.stat.set({stats: wireStat});
-                 //console.log(currentStat);
-                 app.stat.save();
-                 App.updateDisplay();
+              // RECEIVED A MESSAGE.
+               console.log("message received");
+               var wireStat = message;
+               console.log(wireStat);
+               var currentStat = app.stat.get('stats');
+               //console.log(currentStat);
+               var totalHitsOnWire = _.reduce(_.first(wireStat, 4), function(sum, el) {
+                   return sum + el;}, 0);
+
+               var totalHitsCurrent = _.reduce(_.first(currentStat, 4), function(sum, el) {
+                       return sum + el;}, 0);
+
+               if (totalHitsOnWire > totalHitsCurrent ) {
+                         app.stat.set({stats: wireStat});
+                         app.stat.save();
+                         App.updatePopover();
                }
-    },
+              },
+
     connect: function() {
-     pubnub.publish({
-        channel : 'test11',
-        message : app.stat.get('stats'),
-        callback: function(m){ console.log("sent!"); }
-      });},
-      disconnect: function(){console.log("Disconnected");},
-      reconnect: function(){console.log("Reconnected");},
-      error: function(){console.log("Network Error");},};
+               /*pubnub.publish({
+                  channel : 'test11',
+                  message : app.stat.get('stats'),
+                  callback: function(m){ console.log("sent!"); }
+                });*/
+                  pubnub.history({
+                   channel: 'test11',
+                   count: 1,
+                   callback: function(m){
+                    console.log((m[0][0]));
+                    app.stat.set({stats: m[0][0]});
+                    app.stat.save();
+                    App.updatePopover();}
+                  });
+             },
+    disconnect: function(){console.log("Disconnected");},
+    reconnect: function(){console.log("Reconnected");},
+    error: function(){console.log("Network Error");},};
 
   pubnub.subscribe(subscribeObj);
-
-//pubnub.subscribe(_.extend(subscribeObj, {connect: function(){console.log("extended");}}));
 
   $('td:not(:first-child').popover({
   title: '',
@@ -57,14 +66,12 @@ $(function () {
       stats: [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     },
 
-  localStorage: new Backbone.LocalStorage("pcm-stat")
+    initialize: function() {this.fetch();},
+
+    localStorage: new Backbone.LocalStorage("pcm-stat")
   });
 
   app.stat = new app.Stat({id:4890});
-
-  app.stat.fetch();
-
-
 
 
   //Defining Matrix Key Model
@@ -83,7 +90,14 @@ $(function () {
 
     events: {
       'click td': 'updateKey',
-      'click button': 'updateStat'
+      //'click button': 'updateStat'
+      'click button': 'onSubmit'
+    },
+
+    onSubmit: function() {
+      this.updateStat();
+      this.updatePopover();
+      this.publishNow();
     },
 
     publishNow: function() {pubnub.publish({
@@ -98,6 +112,7 @@ $(function () {
 
       this.model.fetch();
       this.updateStat();
+      this.updatePopover();
       this.render();
 
     },
@@ -117,36 +132,28 @@ $(function () {
 
     render: function () {
       var matrixCell = this.model.get('key');
-    //  console.log(matrixCell);
       var cellKey = _.map(matrixCell, function(num, i){ return i * 5 + num; });
-    //  console.log(cellKey);
       _.each(cellKey, this.addClass);
 
     },
 
     updateStat: function() {
       var matrixCell = this.model.get('key');
-      //console.log(matrixCell);
       var currentStat = _.clone(app.stat.get('stats'));
-      //console.log(currentStat);
       _.map(currentStat, increment);
        function increment (value, index){
         ++value[matrixCell[index]-1];
       }
       app.stat.set({stats: currentStat});
-      //console.log(currentStat);
       app.stat.save();
-      this.updateDisplay();
-
     },
 
-    updateDisplay: function() {
+    updatePopover: function() {
       var currentStats = app.stat.get('stats');
       var flatStat = _.flatten(_.map(currentStats, _.values));
       var totalHits = _.reduce(_.first(flatStat, 4), function(sum, el) {
           return sum + el;}, 0).toString();
-      //console.log(totalHits);
-      //console.log(flatStat);
+
       _.map(flatStat, function(data, cell){
         cell = cell;
         var statString = data.toString() + "/" + totalHits;
@@ -154,7 +161,6 @@ $(function () {
         $cell.attr('data-content', statString).data('bs.popover').setContent();
       });
 
-      this.publishNow();
     }
 
   });
